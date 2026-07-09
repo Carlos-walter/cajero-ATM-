23e8ca1fb4fefde39bf0b0c8226c4c0cc58e983e
JHON <lorddexuser@gmail.com>
2026-07-09 17:06:45 -0500

    import de clase MongoManager

---
+++ src/main/java/application/model/MongoManager.java
@@ -0,0 +1,42 @@
+package application.model;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class MongoManager {
+    private static MongoManager instancia;
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+    private MongoManager() {
+        try {
+            String uriAtlas = "mongodb+srv://admin:expoPOO2026@clusterpoo.3z7bvfj.mongodb.net/?appName=ClusterPOO";
+            ConnectionString connectionString = new ConnectionString(uriAtlas);
+
+            MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
+
+            this.mongoClient = MongoClients.create(settings);
+
+            this.database = mongoClient.getDatabase("atm_db");
+
+            System.out.println("¡MongoManager inicializado con éxito apuntando a MongoDB Atlas!");
+        } catch (Exception e) {
+            System.err.println("Error crítico al inicializar la conexión con Atlas: " + e.getMessage());
+        }
+    }
+    public static MongoManager getInstancia() {
+        if (instancia == null) {
+            instancia = new MongoManager();
+        }
+        return instancia;
+    }
+
+    public MongoCollection<Document> getColeccion(String nombre) {
+        return database.getCollection(nombre);
+    }
+}
